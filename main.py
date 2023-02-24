3e4196c831a2b453b855cb4eca9ea8d00cf57d3b
David Taylor <deeteeakl@yahoo.co.nz>
2023-02-24 20:30:25 +0000

    updates

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ if __name__ == "__main__":
         logger.info(f'Weather in Oxford: {temperature}')
 
         my_json = {"message": temperature}
-        r = requests.post(END_POINT, json=my_json)
+        r = requests.post("https://wytham.tk/api/webhook/webhook-test-ETDK-wfnUPlUbcVf2rM28yvz", json=my_json)
